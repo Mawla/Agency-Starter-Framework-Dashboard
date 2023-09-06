3f0882c4a317a24f21f2a332849121d604213458
Arjen <aratramba@gmail.com>
2023-09-06 12:46:59 +0200

    move sign up hook route

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
-
 export default function Page() {
   return (
     <div className="grid place-items-center items-center h-screen">
@@ -8,5 +7,3 @@ export default function Page() {
     </div>
   );
 }
-
- 
