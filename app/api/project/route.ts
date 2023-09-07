641adb65ca6fc87dde1a5c3018d972f50ed0c39f
Arjen <aratramba@gmail.com>
2023-09-07 11:12:56 +0200

    start porting shell script to node

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -3,13 +3,11 @@ import { slugify } from "@/lib/utils";
 import { auth } from "@clerk/nextjs";
 import { groq } from "next-sanity";
 import { NextResponse } from "next/server";
-const exec = require("util").promisify(require("child_process").exec);
 
 export async function POST(_req: Request) {
   const { userId }: { userId: string | null } = auth();
   const res = await _req.json();
   const projectName = slugify(res.projectName);
-  const folderName = `${userId}-${projectName}`;
 
   if (!projectName) {
     return NextResponse.json({
@@ -61,131 +59,132 @@ export async function POST(_req: Request) {
 
   log(`Added project to user ${sanityUserId}`);
 
-  // let p1 = await exec(`
-  //   mkdir -p "/tmp/sgw/${folderName}"
-  //   cd "/tmp/sgw"
-  //   git clone "${process.env.ADMIN_GIT_URL}" "${folderName}"
-  //   cd "./${folderName}"
-  //   rm -rf .git
-  //   touch ".env.development"
-  // `);
-  // console.log(p1.stdout);
-  // console.log(p1.stderr);
+  let q: any;
+  let obj: any;
 
-  // log(`Cloned Git repo`);
-  // log(`Creating new Sanity project`);
+  /**
+   * Create Sanity project
+   */
 
-  // let p2 = await exec(`
-  //   cd "/tmp/sgw/${folderName}"
-  //   result=$(curl --silent POST 'https://api.sanity.io/v2021-06-07/projects' \
-  //     -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" \
-  //     -H 'Content-Type:application/json' \
-  //     -d '{
-  //         "displayName": "${projectName}",
-  //         "organizationId": "${process.env.ADMIN_SANITY_ORGANISATION_ID}"
-  //     }'
-  //   )
+  obj = await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects`,
 
-  //   // TODO: add in sanity project.sanity.id
+    {
+      displayName: projectName,
+      organizationId: process.env.ADMIN_SANITY_ORGANISATION_ID,
+    },
+  );
+  const SANITY_PROJECT_ID = obj.id;
 
-  //   sanityProjectId=$(echo $result 2>&1 | npx --yes groq-cli '(*).id' | xargs)
-  //   echo $sanityProjectId
+  sanityServerClient
+    .patch(projectId)
+    .set({ "sanity.id": SANITY_PROJECT_ID })
+    .commit();
 
-  //   # Write project id to .env.development
-  //   echo "NEXT_PUBLIC_SANITY_PROJECT_ID=$sanityProjectId" >> ".env.development"
-  //   echo "SANITY_STUDIO_API_PROJECT_ID=$sanityProjectId" >> ".env.development"
+  /**
+   * Create Sanity dataset
+   */
 
-  //   # Generate dataset
-  //   echo "Generating new Sanity dataset 'production'"
-  //   dataset=$(curl --silent --request PUT "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/datasets/production" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H 'Content-Type: application/json' -d '{ "aclMode": "public" }')
+  const SANITY_DATASET = "production";
+  obj = await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/datasets/${SANITY_DATASET}`,
+    undefined,
+    "PUT",
+  );
 
-  //   echo "NEXT_PUBLIC_SANITY_DATASET=production" >> ".env.development"
-  //   echo "SANITY_STUDIO_API_DATASET=production" >> ".env.development"
+  /**
+   * Create random tokens
+   */
 
-  //   # Generate preview secret
-  //   echo "Generating preview secret"
-  //   previewSecret=$(curl --silent "https://random-word-api.herokuapp.com/word?number=4" \-H "Accept: application/json" 2>&1 |  npx --yes groq-cli "*[0]+' '+*[1]+' '+*[2]+' '+*[3]" | xargs)
-  //   echo "SANITY_PREVIEW_SECRET=$previewSecret" >> ".env.development"
+  q = await fetch(`https://random-word-api.herokuapp.com/word?number=4`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  obj = await q.json();
+  const SANITY_PREVIEW_SECRET = obj.join("_");
 
-  //   echo "Generating webhook secret"
-  //   webhookSecret=$(curl --silent "https://random-word-api.herokuapp.com/word?number=4" \-H "Accept: application/json" 2>&1 |  npx --yes groq-cli "*[0]+''+*[1]+''+*[2]+''+*[3]" | xargs)
-  //   echo "SANITY_WEBHOOK_SECRET=$webhookSecret" >> ".env.development"
+  q = await fetch(`https://random-word-api.herokuapp.com/word?number=4`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  obj = await q.json();
+  const SANITY_WEBHOOK_SECRET = obj.join("_");
 
-  //   # Generate rest .env.development
-  //   echo "Writing project path"
-  //   echo "SANITY_STUDIO_PROJECT_PATH=/" >> ".env.development"
+  /**
+   * Create Sanity WRITE token
+   */
 
-  //   echo "Generating read api key"
-  //   writeJson=$(curl POST "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/tokens" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H "Content-Type: application/json" --data-raw '{"label":"preview-write","roleName":"editor"}')
-  //   sanityWriteToken=$(echo $writeJson | npx --yes groq-cli '(*).key' | xargs)
-  //   echo "SANITY_API_WRITE_TOKEN=$sanityWriteToken" >> ".env.development"
+  obj = await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/tokens`,
 
-  //   echo "Generating write api key"
-  //   readJson=$(curl POST "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/tokens" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H "Content-Type: application/json" --data-raw '{"label":"preview-read","roleName":"viewer"}')
-  //   sanityReadToken=$(echo $readJson | npx --yes groq-cli '(*).key' | xargs)
-  //   echo "SANITY_API_READ_TOKEN=$sanityReadToken" >> ".env.development"
+    { label: "preview-write", roleName: "editor" },
+  );
+  const SANITY_API_WRITE_TOKEN = obj.key;
 
-  //   # Add CORS origins
-  //   echo "Add localhost CORS origin to Sanity"
-  //   cors1=$(curl --silent --request POST "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/cors" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H 'Content-Type: application/json' -d '{ "origin": "http://localhost:3333", "allowCredentials": true }')
-  //   cors2=$(curl --silent --request POST "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/cors" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H 'Content-Type: application/json' -d '{ "origin": "http://localhost:3000", "allowCredentials": true }')
-  //   cors3=$(curl --silent --request POST "https://api.sanity.io/v2021-06-07/projects/$sanityProjectId/cors" -H "Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}" -H 'Content-Type: application/json' -d '{ "origin": "https://${projectName}.vercel.app", "allowCredentials": true }')
-  // `);
+  /**
+   * Create Sanity READ token
+   */
 
-  // TODO invite Sanity users
+  obj = await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/tokens`,
 
-  // console.log(p2.stdout);
-  // console.log(p2.stderr);
+    { label: "preview-read", roleName: "viewer" },
+  );
+  const SANITY_API_READ_TOKEN = obj.key;
 
-  // log(`Done creating Sanity project`);
-  console.log(`Set up Vercel project`);
+  /**
+   * Create Sanity CORS origins
+   */
 
-  let buildProcess = await exec(`
+  await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/cors`,
 
-    # ---
-    # Create new Vercel project
-    result=$(curl -X POST "https://api.vercel.com/v9/projects?teamId=${process.env.ADMIN_VERCEL_TEAM_ID}" \
-      -H "Authorization: Bearer ${process.env.ADMIN_VERCEL_API_TOKEN}" \
-      -H "Content-Type: application/json" \
-      -d '{
-        "name": "${projectName}",
-        "commandForIgnoringBuildStep": "if [ \\"$VERCEL_ENV\\" == \\"production\\" ]; then exit 1; else exit 0; fi",
-        "environmentVariables": [
-          {
-            "key": "TEST_encrypted",
-            "target": "production",
-            "type": "encrypted",
-            "value": "SOME_STRING_VALUE"
-          }
-        ],
-        "framework": "nextjs",
-        "gitRepository": {
-          "repo": "${process.env.ADMIN_GITHUB_REPO}",
-          "type": "github"
-        },
-        "serverlessFunctionRegion": "dub1"
-      }'
-  )
-  echo $result
-  vercelProjectId=$(echo $result 2>&1 | npx --yes groq-cli '(*).id' | xargs)
-  echo $vercelProjectId
+    {
+      origin: "http://localhost:3333",
+      allowCredentials: true,
+    },
+  );
 
-  # ---
-  # store vercel id in cms
-  curl --request POST 'https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/data/mutate/production' \
-    -H 'Authorization: Bearer ${process.env.ADMIN_SANITY_TOKEN}' \
-    -H 'Content-Type: application/json' \
-    -d "{ \\"mutations\\": [{ \\"patch\\": { \\"id\\": \\"${projectId}\\", \\"set\\": { \\"vercel.id\\": \\"$vercelProjectId\\" }}}] }"
+  await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/cors`,
 
-`);
+    {
+      origin: "http://localhost:3000",
+      allowCredentials: true,
+    },
+  );
 
-  console.log(buildProcess.stdout);
-  console.log(buildProcess.stderr);
+  await sanityFetch(
+    `https://api.sanity.io/v2021-06-07/projects/${SANITY_PROJECT_ID}/cors`,
 
-  // TODO: can we just remove whole tmp dir logic??
-  // await exec(`rm -rf "/tmp/sgw/${folderName}"`);
+    {
+      origin: `https://${projectName}.vercel.app`,
+      allowCredentials: true,
+    },
+  );
+
+  console.log(SANITY_PROJECT_ID);
+  console.log(SANITY_DATASET);
+  console.log(SANITY_PREVIEW_SECRET);
+  console.log(SANITY_WEBHOOK_SECRET);
+  console.log(SANITY_API_WRITE_TOKEN);
+  console.log(SANITY_API_READ_TOKEN);
 
   return NextResponse.json({
     ok: 1,
   });
+}
+
+async function sanityFetch(url: string, body: any, method = "POST") {
+  console.log(url, body);
+  const params: any = {
+    headers: {
+      Authorization: `Bearer ${process.env.ADMIN_SANITY_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    method,
+  };
+  if (body) params.body = JSON.stringify(body);
+
+  const res = await fetch(url, params);
+  const obj = await res.json();
+  return obj;
 }
