7cf13d9f15fa2857d988c58c83c7fbfbcb1f3a56
Arjen <aratramba@gmail.com>
2023-09-07 14:36:55 +0200

    Update Spinner.tsx

--- components/ui/Spinner.tsx
+++ components/ui/Spinner.tsx
@@ -1,8 +1,8 @@
-export default function Spinner() {
+export default function Spinner({ size }: { size: number }) {
   return (
     <svg
-      width={24}
-      height={24}
+      width={size}
+      height={size}
       fill="currentColor"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
