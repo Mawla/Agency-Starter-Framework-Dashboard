11133c0681c4da1fc7dedaab9fa214c1caf2cc7f
Arjen <aratramba@gmail.com>
2023-09-19 16:56:43 +0200

    Update export-import-dataset.ts

--- app/api/create-project/export-import-dataset.ts
+++ app/api/create-project/export-import-dataset.ts
@@ -19,7 +19,7 @@ export async function exportImportDataset({
   log: (message: string) => void;
 }) {
   const queue = new PQueue({
-    concurrency: 25,
+    concurrency: 20,
     interval: 1000,
   });
 
